245150f2c57bde7fa72f713ec4e61da365d5ceaa
Furkan Vural <furkanvural52@gmail.com>
2023-03-19 00:05:34 +0300

    Connected UIcomponents with OnboardingVC

--- DsmDictionary/DsmDictionary/ViewController.swift
+++ DsmDictionary/DsmDictionary/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var onboardingImage: UIImageView!
+    
+    @IBOutlet weak var firstDefinitionLabel: UILabel!
+    
+    @IBOutlet weak var secondDefinitionLabel: UILabel!
+    
+    @IBOutlet weak var thirdDefinitionLabel: UILabel!
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStartedButton(_ sender: Any) {
+    }
+    
 }
 
