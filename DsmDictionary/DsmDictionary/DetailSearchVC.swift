bbb1965502c7aded496cd906f8ceb06fb4908e02
Furkan Vural <furkanvural52@gmail.com>
2023-03-23 00:18:45 +0300

    Added tableView and searchBar delegate

--- DsmDictionary/DsmDictionary/DetailSearchVC.swift
+++ DsmDictionary/DsmDictionary/DetailSearchVC.swift
@@ -14,11 +14,38 @@ class DetailSearchVC: UIViewController {
     @IBOutlet weak var searchResultTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureDetailSearchPageView()
       
+    }
+    
+    func configureDetailSearchPageView(){
+        self.searchResultTableView.delegate = self
+        self.searchResultTableView.dataSource = self
+        self.searchBar.delegate = self
     }
     
 
     
 
+}
+
+extension DetailSearchVC: UISearchBarDelegate {
+    
+}
+
+extension DetailSearchVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "PlaceHolder"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
 }
