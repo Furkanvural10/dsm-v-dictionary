b0754b16381c0daaac1b9efb983d9147476bf2db
Furkan Vural <furkanvural52@gmail.com>
2023-03-23 00:07:20 +0300

    Added DetailSearchPage file and UI elements

---
+++ DsmDictionary/DsmDictionary/DetailSearchVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailSearchVC.swift
+//  DsmDictionary
+//
+//  Created by furkan vural on 23.03.2023.
+//
+
+import UIKit
+
+class DetailSearchVC: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var searchResultTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    
+
+}
